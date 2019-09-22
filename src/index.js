755802f86d593264c059ef5eedb892896ca98626
Dheeraj Suthar <dheersuthar@gmail.com>
2019-09-22 22:39:47 +0530

    Basic scrumboard

--- src/index.js
+++ src/index.js
@@ -89,6 +89,30 @@ function runDetection() {
     });
 }
 
+//Scrumboard
+$(function() {
+    $( ".column" ).sortable({
+      connectWith: ".column",
+      handle: ".portlet-header",
+      cancel: ".portlet-toggle",
+      placeholder: "portlet-placeholder ui-corner-all"
+    });
+ 
+    $( ".portlet" )
+      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".portlet-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+       
+ 
+    $( ".portlet-toggle" ).click(function() {
+        console.log('here');
+        
+      var icon = $( this );
+      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+      icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+    });
+  });
+//End scrumboard
 // Load the model.
 handTrack.load(modelParams).then(lmodel => {
     // detect objects in the image.
